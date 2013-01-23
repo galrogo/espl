17dd121ef074ea3acb3fac82eb044bfd13c66240
Gal Rogozinski <galrogo@post.bgu.ac.il>
2013-01-23 15:36:21 +0200

    final

--- lab11/index.js
+++ lab11/index.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+ 
     //only numbers in user field
     document.getElementById('user_name').onkeydown = function(event){
     return (!(event.keyCode>=48 && event.keyCode<=57))  
@@ -12,27 +13,56 @@ window.onload = function() {
 //      event.preventDefault();
 //      return false;  
 //      }
- };
- 
+ }
+    //ensure the first character of password is a letter
     document.getElementById('password').onkeydown = function(event){
       if (this.value.length === 0){
 	return ((event.keyCode>=64 && event.keyCode<=90) || (event.keyCode>=97 && event.keyCode<=122));
       }
-    } 
+    }
     
+      //ensure that upon submission
      document.getElementById('login_form').onsubmit = function(event){
-      if (this.getElementById('user name').value.length === 0){
+	//user name isn't empty
+
+      if (document.getElementById('user_name').value.length === 0){
 // 	    alert("no user name");
 	    return false;
       }
-      
-      if (this.getElementById('password').value.length >= 4){
-	alert("password must have 4 characters");
-	return false;
-      }
-      
-     }    
-}
-    
- 
 
+      
+// 	//ensure password is at least 4 characters long
+      if (document.getElementById('password').value.length < 4){
+	    alert("password must have 4 characters");
+	    return false;
+      }
+
+     
+      document.getElementById('paragraph').innerHTML = "you are now logged in as " + document.getElementById('user_name').value;
+
+      // don't actually submit the form
+      return false;
+     }
+}
+
+function showHint(str)
+{
+	var xmlhttp;	
+        if (str.length === 0) {
+            document.getElementById("txtHint").innerHTML = "";
+            return;
+        }
+        if (window.XMLHttpRequest){ // IE7+,Firefox,Chrome,Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        }
+        else{ // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status === 200){
+                document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
+            }
+        }
+        xmlhttp.open("GET","http://www.cs.bgu.ac.il/~galrogo/suggest.php?q="+str, true);
+        xmlhttp.send();
+}
